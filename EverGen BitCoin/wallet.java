0533925f16fbb91335b52d42108094ea7a74f29c
EverGen1 <nextgenerationever@gmail.com>
2015-12-13 19:14:40 -0700

    update

--- EverGen BitCoin/wallet.java
+++ EverGen BitCoin/wallet.java
@@ -1,2 +1,16 @@
 import co.gem.*;
-public class GettingStarted
+public class GettingStarted{
+ public static void main(String[] args) throws IOException,
+ Client.UnexpectedStatusCodeException{
+  Round client = Round.client(“https://api.gem.co”);
+  User.Wrapper newUser = null;
+   try{
+    newUser = client.users().create(“admin@evergencompany.com”, “pass-word”, “testate”);
+   }catch (InvalidKeySpecException e){
+    e.printStackTrace();
+  }catch (NoSuchAlgorithmException e)(
+   e.printStackTrace();
+  }
+  System.out.printIn(“Backup Seed: “, + newUser.backupPrivateSeed);
+  String key = 
+}
